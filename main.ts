7a5ac3fdfec4ef2957f4b8ae1ac0cc284519cdf5
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-10-10 16:16:03 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(100)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
-    music.playTone(330, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
-    music.playTone(262, music.beat(BeatFraction.Whole))
 })
 radio.setGroup(1)
